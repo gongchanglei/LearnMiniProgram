9af93e0f28c2c6899caa368c2444f376dd7f4099
gcl283666 <gcl283666>
2020-11-11 11:07:14 +0800

    知识点一

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,23 +1,28 @@
-// pages/home/home.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-
+Page({
+  data : {
+    name : "gcl",
+    age : 18,
+    students :[
+      {id:100,name:'小明',age:18},
+      {id:101,name:'小红',age:19},
+      {id:102,name:'小强',age:20},
+      {id:103,name:'小刘',age:21}
+    ],
+    counter:0
   },
+  handleBtnClick(){
+    // counter增加了，但界面没有刷新
+    // this.data.counter += 1
+    // console.log(this.data.counter)
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
-
+    // 使用setData()
+    this.setData({
+      counter:this.data.counter + 1
+    })
   },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
+  handleBtnClick2(){
+    this.setData({
+      counter:this.data.counter - 1
+    })
   }
 })
