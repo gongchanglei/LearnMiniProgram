e9b2e4832f698f0a59db2734fb8653d938d88f4c
gcl283666 <gcl283666>
2020-12-01 14:44:23 +0800

    11_系统APIPI—_网络请求

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,23 +1,61 @@
 // pages/home/home.js
-Component({
-  /**
-   * 组件的属性列表
-   */
-  properties: {
+import request from '../../service/network.js'
+Page({
+  data:{
 
   },
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
+  onLoad:function(options){
+    // this.get_data_origin()
 
+    // 使用封装的request发送网络请求
+    // promise请求方式
+    request({
+      url:'http://123.207.32.32:8000/recommend',
+    }).then(res=> {
+      console.log(res)
+    }).catch(err=> {
+      console.log(err)
+    })
+  
   },
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
+  get_data_origin(){
+      
+    // 发送网络请求 
+
+    // 1 不带参数的get请求
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/recommend',
+    //   success:function(res){
+    //     console.log(res)
+    //   }
+    // })
+
+    // 2 带参数的get请求 
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/home/data',
+    //   data:{
+    //     type:'sell',
+    //     page:1
+    //   },
+    //   success:function(res){
+    //     console.log(res)
+    //   }
+    // }),
+
+    // 带参数的post请求
+    wx.request({
+      url: 'http://httpbin.org/post',
+      method:'post',
+      data:{
+        type:'sell', 
+        page:1
+      },
+      success:function(res){
+        console.log(res)
+      }
+    })
 
   }
 })
